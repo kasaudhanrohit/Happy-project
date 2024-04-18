4376a444df907f56dc426534b90d8dd02e1ba27f
Rohit Kasaudhan <rohit.kasaudhan@cavisson.com>
2024-04-18 19:40:04 +0530

    my_order

--- src/app/my-cart/my-cart.component.ts
+++ src/app/my-cart/my-cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttprequestService } from '../common-service/httprequest.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-my-cart',
   templateUrl: './my-cart.component.html',
@@ -7,19 +8,26 @@ import { HttprequestService } from '../common-service/httprequest.service';
 })
 export class MyCartComponent implements OnInit {
   busy = true;
-  constructor(private httpreq : HttprequestService) { }
+  myorderdata = [];
+  constructor(private router:Router,private httpreq : HttprequestService) { }
 
   ngOnInit(): void {
     this.busy = false;
+    this.myorderdata = [];
     let logedinuserinfo = JSON.parse(localStorage.getItem("userinfo"));
     this.httpreq.getusercartinfo({"username":logedinuserinfo.username}).subscribe((data )=>
       { 
         console.log("getuserorderinfo response : " ,data)
         if(data && data[0] && data[0]['status'] == "success")
           {
+            this.myorderdata = data[0]['data'];
             console.log("data[0][data] ",data[0]['data']);
           }
       });
+  }
+  openproductdetail(productname)
+  {
+    this.router.navigate(['/product-detail'], { queryParams: { productitem_name: productname } });
   }
 
 }
--- src/app/my-cart/my-cart.module.ts
+++ src/app/my-cart/my-cart.module.ts
@@ -5,11 +5,14 @@ import { LoaderModule } from '../loader/loader.module';
 import { NavBarModule } from '../nav-bar/nav-bar.module';
 import { RouterModule, Routes } from '@angular/router';
 import { MyCartComponent } from './my-cart.component';
+import {TableModule} from 'primeng/table';
+
 const routes: Routes = [
   { path: '', component: MyCartComponent }
 ];
 
 const imports = [
+  TableModule,
   CommonModule,
   AboutInfoModule,
   LoaderModule,
